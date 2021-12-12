f731154c3145d256d0fda1c4ee977ab7e01348bc
ngdloc2000 <ngdinhloc2000@gmail.com>
2021-12-12 11:56:42 +0700

    bt

---
+++ Buoi_17/thuc-hanh-spring-boot/src/test/java/com/example/thuchanhspringboot/ThucHanhSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thuchanhspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThucHanhSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
